914a449523681661c4db89488c7d9cb217558e27
shettyjj <152796044+shettyjj@users.noreply.github.com>
2024-03-10 08:05:52 +0000

    dymaic data 1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return render_template('etheral.html')
+  return render_template('etheral.html',name='Temperature controlled Fan')
 
 
 if __name__ == "__main__":
