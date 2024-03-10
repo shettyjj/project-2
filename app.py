ddaa334e7cb53ab16bd3634f051af21d5bd70df0
shettyjj <152796044+shettyjj@users.noreply.github.com>
2024-03-10 15:11:43 +0000

    interface

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return render_template('etheral.html',name='Temperature controlled Fan')
-
-
+  return render_template('etheral.html')
+@app.route("/add")
+def add():
+    return render_template("add.html")
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
